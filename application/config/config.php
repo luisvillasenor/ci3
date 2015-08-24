0ed598c8a053aaf384c9b5decb87eb19508458c1
= <luisgvillasenor@yahoo.com>
2015-08-23 19:42:47 -0500

    configurar template

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/ci3/';
+$config['base_url'] = 'http://luisgvillasenor/ci3/';
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ $config['base_url'] = 'http://localhost/ci3/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,38 +1,14 @@
     <div class="container-fluid">
       <div class="">
-
                
           <div class="jumbotron">
             <h1>Psicometria</h1>
-            <p></p>
+            <p>Que obtienes?</p>
             <p>
               <a class="btn btn-primary btn-large" href="gestalt">Iniciar</a>
             </p>
           </div>
         
-          <div class="row placeholders">
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Test 1</h4>
-              <span class="text-muted">Autoestima</span>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Test 2</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Test 3</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Test 4</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-          </div>
-
         </div>
 
       </div>
